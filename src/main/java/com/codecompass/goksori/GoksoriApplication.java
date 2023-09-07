f4a7a51bd58837bc4043e8cac0515e26868b99c3
dh.kang <dh.kang@navercorp.com>
2023-09-07 21:06:25 +0900

    initialize project

---
+++ src/main/java/com/codecompass/goksori/GoksoriApplication.java
@@ -0,0 +1,11 @@
+package com.codecompass.goksori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoksoriApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(GoksoriApplication.class, args);
+    }
+}
---
+++ src/test/java/com/codecompass/goksori/GoksoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecompass.goksori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoksoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
