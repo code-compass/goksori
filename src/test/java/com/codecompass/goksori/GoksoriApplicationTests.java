3123191187e8440c59457845e193a6f40ddd8d96
DonghoonKang <68961601+donghoon-khan@users.noreply.github.com>
2023-09-07 23:10:24 +0900

    fix test coverage action (#2)
    * fix test coverage action
    ---------
    
    Co-authored-by: dh.kang <dh.kang@navercorp.com>

--- src/test/java/com/codecompass/goksori/GoksoriApplicationTests.java
+++ src/test/java/com/codecompass/goksori/GoksoriApplicationTests.java
@@ -1,13 +1,20 @@
 package com.codecompass.goksori;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class GoksoriApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void testMain() {
+        try (final MockedStatic<SpringApplication> mockedStatic = mockStatic(SpringApplication.class)) {
+            final var args = new String[]{"arg1", "arg2"};
+            GoksoriApplication.main(args);
+            mockedStatic.verify(() -> SpringApplication.run(GoksoriApplication.class, args));
+        }
+    }
 }
